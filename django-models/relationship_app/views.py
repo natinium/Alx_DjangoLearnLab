c964d1fedc38ae7db6aa3cf4318dad355bac561a
Natnael Mesele <nmesele5@gmail.com>
2024-11-10 22:25:19 +0300

    Implementing User Authentication

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from .models import Book
+from django.contrib.auth import login
+from django.views.generic import DetailView
+from .models import Book, Library
 
 def list_books(request):
     books = Book.objects.all()
@@ -15,8 +17,9 @@ def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('login')  # Redirect to login after successful registration
+            user = form.save()
+            login(request, user)  # Log in the user immediately after registration
+            return redirect('home')  # or wherever you want to redirect after login
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
