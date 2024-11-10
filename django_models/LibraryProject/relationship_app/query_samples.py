8a2b36504bf282338c9b41f9ac48b1de982fdee3
Natnael Mesele <nmesele5@gmail.com>
2024-11-10 21:29:17 +0300

    create and add the relationship app

---
+++ django_models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,29 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+def get_books_by_author(author_name):
+    try:
+        author = Author.objects.get(name=author_name)
+        books = Book.objects.filter(author=author)
+        print(f"Books by {author.name}:")
+        for book in books:
+            print(f"- {book.title}")
+    except Author.DoesNotExist:
+        print("Author does not exist.")
+
+def list_books_in_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        books = library.books.all()
+        print(f"Books in {library.name} library:")
+        for book in books:
+            print(f"- {book.title} by {book.author.name}")
+    except Library.DoesNotExist:
+        print("Library does not exist.")
+
+def get_librarian_for_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        librarian = Librarian.objects.get(library=library)
+        print(f"Librarian of {library.name} library is {librarian.name}")
+    except (Library.DoesNotExist, Librarian.DoesNotExist):
+        print("Library or librarian does not exist.")
