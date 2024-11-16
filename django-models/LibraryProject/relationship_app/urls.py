7966d31717d860c8cc10c8a6f24258ad409df499
Natnael Mesele <nmesele5@gmail.com>
2024-11-16 22:48:15 +0300

    fix detailview error

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
@@ -10,4 +11,6 @@ urlpatterns = [
     path('admin-view/', views.admin_view, name='admin_view'),
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
+    path('books/fbv/', list_books, name='list_books_fbv'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
