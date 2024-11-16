94c01a54231aa8fcde1e8e528c41a1a60d103c3b
Natnael Mesele <nmesele5@gmail.com>
2024-11-16 22:33:53 +0300

    fix librarian and memebr views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -42,12 +42,12 @@ def is_member(user):
 
 @user_passes_test(is_admin, login_url='login')
 def admin_view(request):
-    return render(request, 'relationship_app/admin_page.html')
+    return render(request, 'relationship_app/admin_view.html')
 
 @user_passes_test(is_librarian, login_url='login')
 def librarian_view(request):
-    return render(request, 'relationship_app/librarian_page.html')
+    return render(request, 'relationship_app/librarian_view.html')
 
 @user_passes_test(is_member, login_url='login')
 def member_view(request):
-    return render(request, 'relationship_app/member_page.html')
+    return render(request, 'relationship_app/member_view.html')
