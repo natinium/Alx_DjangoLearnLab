8cae9cba1b6091e98076d7bb9cc8b565846f416d
Natnael Mesele <nmesele5@gmail.com>
2024-11-16 23:06:04 +0300

    fix error in urls list

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     path('admin-view/', views.admin_view, name='admin_view'),
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
-    path('books/fbv/', list_books, name='list_books_fbv'),
+    path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('add-book/', views.add_book, name='add_book'),
-    path('edit-book/<int:pk>/', views.edit_book, name='edit_book'),
-    path('delete-book/<int:pk>/', views.delete_book, name='delete_book'),
+    path('edit-book/', views.edit_book, name='edit_book'),
+    path('delete-book/', views.delete_book, name='delete_book'),
 ]
