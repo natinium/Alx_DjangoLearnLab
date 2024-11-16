ea5618f4d3ae3707eca492e4d0867e8037d26f8f
Natnael Mesele <nmesele5@gmail.com>
2024-11-16 23:11:02 +0300

    fix add_book view error

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, add_book, edit_book
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
@@ -13,7 +13,7 @@ urlpatterns = [
     path('member-view/', views.member_view, name='member_view'),
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('add-book/', views.add_book, name='add_book'),
-    path('edit-book/', views.edit_book, name='edit_book'),
+    path('add-book/', add_book, name='add_book'),
+    path('edit-book/', edit_book, name='edit_book'),
     path('delete-book/', views.delete_book, name='delete_book'),
 ]
