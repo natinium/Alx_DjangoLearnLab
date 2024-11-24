5d1ad087867942f4d6d24e2be24fba1c3c3cb445
Natnael Mesele <nmesele5@gmail.com>
2024-11-24 13:20:15 +0300

    create the api endpoint and add it to the app

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    permission_classes = [AllowAny]
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
