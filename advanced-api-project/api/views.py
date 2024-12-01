d98575f624cc444f30b43019ef677e42c6dad692
Natnael Mesele <nmesele5@gmail.com>
2024-12-01 11:10:00 +0300

    add permissions to the api

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class ListView(generics.ListAPIView):
+    """
+    Retrieves a list of all books.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class DetailView(generics.RetrieveAPIView):
+    """
+    Retrieves a single book by its ID.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class CreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get('title')
+        if Book.objects.filter(title=title).exists():
+            raise ValidationError({"detail": "A book with this title already exists."})
+        serializer.save()
+
+class UpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_update(self, serializer):
+        publication_year = serializer.validated_data.get('publication_year')
+        if publication_year > 2024:
+            raise ValidationError({"detail": "Publication year cannot be in the future."})
+        serializer.save()
+
+
+class DeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
