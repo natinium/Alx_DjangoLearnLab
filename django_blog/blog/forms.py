c5db4d828cdee17fd2331ce381cc5af54e017d25
Natnael Mesele <nmesele5@gmail.com>
2024-12-08 17:05:41 +0300

    fix some issues

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,15 +1,30 @@
 from django import forms
 from .models import Post, Comment
-from taggit.forms import TagField, TagWidget  # Import TagWidget
+from taggit.forms import TagField, TagWidget
 
 class PostForm(forms.ModelForm):
-    tags = forms.CharField(widget=TagWidget(), required=False)  # Use TagWidget for tags
+    tags = forms.CharField(
+        widget=TagWidget(attrs={'class': 'form-control'}), 
+        required=False
+    )
+
+    title = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter the title'})
+    )
+
+    content = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write your content here'})
+    )
 
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
 
 class CommentForm(forms.ModelForm):
+    content = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write your comment here'})
+    )
+
     class Meta:
         model = Comment
         fields = ['content']
