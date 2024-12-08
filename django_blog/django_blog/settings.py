d020abb52dbf004d13ec4c381032bf80306036a0
Natnael Mesele <nmesele5@gmail.com>
2024-12-08 12:27:09 +0300

    add the postgresql to the app

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -79,9 +79,14 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_db',
+        'USER': 'blog_db_admin', 
+        'PASSWORD': '123456789',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
