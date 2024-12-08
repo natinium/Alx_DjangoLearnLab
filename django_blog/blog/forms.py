7f855b63939980403717fdef163e39da8b25714f
Natnael Mesele <nmesele5@gmail.com>
2024-12-08 14:45:16 +0300

    fix tag functionality error

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,14 +1,13 @@
 from django import forms
 from .models import Post, Comment
-from taggit.forms import TagField
+from taggit.forms import TagField, TagWidget  # Import TagWidget
 
 class PostForm(forms.ModelForm):
-    tags = forms.CharField(widget=TagWidget(), required=False)
+    tags = forms.CharField(widget=TagWidget(), required=False)  # Use TagWidget for tags
 
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
-
 
 class CommentForm(forms.ModelForm):
     class Meta:
