f8023a550440a265050ff08636b5864d2a80981c
Natnael Mesele <nmesele5@gmail.com>
2024-12-15 21:55:14 +0300

    Implementing Notifications and Likes Functionality

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -14,3 +14,10 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'post')
