3943634f032d51fa369842ee4b326167ef4bac51
Natnael Mesele <nmesele5@gmail.com>
2024-12-15 21:40:02 +0300

    Fix error in User Follows and Feed Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-from rest_framework.decorators import api_view
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
@@ -21,7 +22,7 @@ class CommentViewSet(viewsets.ModelViewSet):
 
 @api_view(['GET'])
 def user_feed(request):
-    followed_users = request.user.following.all()
-    posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+    following_users = request.user.following.all()
+    posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
