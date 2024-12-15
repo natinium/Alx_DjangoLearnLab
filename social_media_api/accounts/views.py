c6a58b8774b8f8cfadd9d315896811f70180f830
Natnael Mesele <nmesele5@gmail.com>
2024-12-15 21:29:53 +0300

    Implementing User Follows and Feed Functionality

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics,  permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
@@ -16,14 +16,19 @@ class LoginView(ObtainAuthToken):
         response = super(LoginView, self).post(request, *args, **kwargs)
         token = response.data['token']
         return Response({'token': token})
-@api_view(['POST'])
-def follow_user(request, user_id):
-    user_to_follow = User.objects.get(id=user_id)
-    request.user.following.add(user_to_follow)
-    return Response({"message": "Followed successfully."})
 
-@api_view(['POST'])
-def unfollow_user(request, user_id):
-    user_to_unfollow = User.objects.get(id=user_id)
-    request.user.following.remove(user_to_unfollow)
-    return Response({"message": "Unfollowed successfully."})
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        user_to_follow = get_object_or_404(User, id=user_id)
+        request.user.following.add(user_to_follow)
+        return Response({'message': f'You are now following {user_to_follow.username}.'})
+
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        user_to_unfollow = get_object_or_404(User, id=user_id)
+        request.user.following.remove(user_to_unfollow)
+        return Response({'message': f'You have unfollowed {user_to_unfollow.username}.'})
