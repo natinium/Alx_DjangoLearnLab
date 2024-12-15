e1b3bff9d14ed91f0051e1fd6f690ed5af8500e8
Natnael Mesele <nmesele5@gmail.com>
2024-12-15 16:09:20 +0300

    fix user serializer error

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
-from .models import User
+from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True)
