c1545cb2ac452192dddaf372154c6134a0e6fe61
Natnael Mesele <nmesele5@gmail.com>
2024-12-15 16:06:57 +0300

    fix user serializer error

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,13 +3,18 @@ from .models import User
 from rest_framework.authtoken.models import Token
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = User
         fields = ['id', 'username', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = User(**validated_data)
-        user.set_password(validated_data['password'])
-        user.save()
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture')
+        )
         Token.objects.create(user=user)
         return user
